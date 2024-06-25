5435a40b66391ac4af40cb112c9500d032b1d42f
Waris <warisareshi@gmail.com>
2024-06-25 07:33:32 +0530

    Sign In Route Added

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+
+
+const signInPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+        Sign In Page
+    </div>
+  );
+};
+
+export default signInPage;
