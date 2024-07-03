238297d3498a04ee4dd9f595a9f3e494eff7f70b
Waris <warisareshi@gmail.com>
2024-07-03 06:59:38 +0530

    Minor

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -123,7 +123,7 @@ function UserDashboard() {
   const { username } = session.user as User;
 
   const baseUrl = `${window.location.protocol}//${window.location.host}`;
-  const profileUrl = `${baseUrl}/u/${username}`;
+  const profileUrl = `${baseUrl}/user/${username}`;
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(profileUrl);
